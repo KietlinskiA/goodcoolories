d0c2157fda67090462f2441a5bf6c24fff4cbff9
KietlinskiA <adrian.kietlinski@interia.pl>
2021-11-22 12:54:10 +0100

    Add new templates

--- src/main/java/pl/kietlinski/goodcoolories/Entity/Order.java
+++ src/main/java/pl/kietlinski/goodcoolories/Entity/Order.java
@@ -5,9 +5,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 
 @Getter
 @Setter
@@ -22,7 +19,6 @@ public class Order {
     @Column(length = 100, nullable = false)
     private String name;
     @Column(length = 50, nullable = false)
-    @Email
     private String eaddress;
     @Column(length = 9, nullable = false)
     private String phone;
@@ -40,14 +36,19 @@ public class Order {
     private String sex;
     @Column(length = 6, nullable = false)
     private String activity;
+    @Column(length = 4, nullable = false)
+    private String dietWish;
     @Column(length = 1, nullable = false)
-    @Min(3)
-    @Max(6)
     private int dishCount;
     @Column(length = 1000, nullable = false)
     private String comment;
+    @Column(length = 10, nullable = false)
+    private String status;
 
-    public Order(String name, String eaddress, String phone, String street, String zip, String city, int age, int height, String sex, String activity, int dishCount, String comment) {
+    @OneToOne
+    private Diet diet;
+
+    public Order(String name, String eaddress, String phone, String street, String zip, String city, int age, int height, String sex, String activity, String dietWish, int dishCount, String comment) {
         this.name = name;
         this.eaddress = eaddress;
         this.phone = phone;
@@ -58,8 +59,10 @@ public class Order {
         this.height = height;
         this.sex = sex;
         this.activity = activity;
+        this.dietWish = dietWish;
         this.dishCount = dishCount;
         this.comment = comment;
+        this.status = "Nowy";
     }
 
     @Override
@@ -76,8 +79,11 @@ public class Order {
                 ", height=" + height +
                 ", sex='" + sex + '\'' +
                 ", activity='" + activity + '\'' +
+                ", dietWish='" + dietWish + '\'' +
                 ", dishCount=" + dishCount +
                 ", comment='" + comment + '\'' +
+                ", status='" + status + '\'' +
+                ", diet=" + diet.getDietId() +
                 '}';
     }
 }
