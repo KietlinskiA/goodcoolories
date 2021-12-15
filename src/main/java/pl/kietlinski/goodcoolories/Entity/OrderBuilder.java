9d861533603869df0349b76379df61bd886fe68e
KietlinskiA <adrian.kietlinski@interia.pl>
2021-12-15 15:13:02 +0100

    Create builder Order class Add new ico with white background

---
+++ src/main/java/pl/kietlinski/goodcoolories/Entity/OrderBuilder.java
@@ -0,0 +1,86 @@
+package pl.kietlinski.goodcoolories.Entity;
+
+public class OrderBuilder {
+    private String name;
+    private String eaddress;
+    private String phone;
+    private String street;
+    private String zip;
+    private String city;
+    private int age;
+    private int height;
+    private String sex;
+    private String activity;
+    private String dietWish;
+    private int dishCount;
+    private String comment;
+
+    public OrderBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public OrderBuilder setEaddress(String eaddress) {
+        this.eaddress = eaddress;
+        return this;
+    }
+
+    public OrderBuilder setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public OrderBuilder setStreet(String street) {
+        this.street = street;
+        return this;
+    }
+
+    public OrderBuilder setZip(String zip) {
+        this.zip = zip;
+        return this;
+    }
+
+    public OrderBuilder setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public OrderBuilder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public OrderBuilder setHeight(int height) {
+        this.height = height;
+        return this;
+    }
+
+    public OrderBuilder setSex(String sex) {
+        this.sex = sex;
+        return this;
+    }
+
+    public OrderBuilder setActivity(String activity) {
+        this.activity = activity;
+        return this;
+    }
+
+    public OrderBuilder setDietWish(String dietWish) {
+        this.dietWish = dietWish;
+        return this;
+    }
+
+    public OrderBuilder setDishCount(int dishCount) {
+        this.dishCount = dishCount;
+        return this;
+    }
+
+    public OrderBuilder setComment(String comment) {
+        this.comment = comment;
+        return this;
+    }
+
+    public Order createOrder() {
+        return new Order(name, eaddress, phone, street, zip, city, age, height, sex, activity, dietWish, dishCount, comment);
+    }
+}
